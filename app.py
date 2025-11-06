7c6e11831eb016d007ea044d4ea7c01207fc61f6
Rahat Naqvi <rahatnaqvi@gmail.com>
2025-11-06 06:21:53 +0400

    My local Flask CI/CD updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask CI/CD Pipeline (Jenkins → Docker → Kubernetes)"
+    return "Hello from Flask CI/CD Pipeline created by Rahat Naqvi (Jenkins → Docker → Kubernetes)"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
