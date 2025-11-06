765a769d0e31d659b4b5df7906a7fca59e36f28e
Rahat Naqvi <rahatnaqvi@gmail.com>
2025-11-06 06:31:24 +0400

    Updated base image and port

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Hello from Flask CI/CD Pipeline created by Rahat Naqvi (Jenkins → Docker → Kubernetes)"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
+
